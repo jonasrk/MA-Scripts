f68112d9f2872e69a0138324c5663c0183199882
jonas.kemper <jonas.kemper.biz@gmail.com>
2017-06-13 10:35:04 +0200

    updates

--- generate_select_repo.py
+++ generate_select_repo.py
@@ -9,8 +9,8 @@ all_measurements = {}
 for i in range(0, nr_items):
 	key = content[i]
 	if key in all_measurements:
-		all_measurements[key]['in'].append(content[i+nr_items])
-		all_measurements[key]['out'].append(content[i+2*nr_items])
+		all_measurements[key]['in'].append(int(content[i+nr_items]))
+		all_measurements[key]['out'].append(int(content[i+2*nr_items]))
 	else:
 		all_measurements[key] = {}
 		all_measurements[key]['in'] = [int(content[i+nr_items])]
@@ -19,13 +19,13 @@ for i in range(0, nr_items):
 for key in all_measurements:
 	print(key + " = {\\")
 	print('  "p":1,\\ ')
-	min = sys.maxint
-	max = 0
+	min = sys.float_info.max
+	max = 0.0
 	for i in range(0, len(all_measurements[key]['in'])):
-		if all_measurements[key]['out'][i] / all_measurements[key]['in'][i] < min:
-			min = all_measurements[key]['out'][i] / all_measurements[key]['in'][i]
-		if all_measurements[key]['out'][i] / all_measurements[key]['in'][i] > max:
-			max = all_measurements[key]['out'][i] / all_measurements[key]['in'][i]
+		if 1.0 * all_measurements[key]['out'][i] / all_measurements[key]['in'][i] < min:
+			min = 1.0 * all_measurements[key]['out'][i] / all_measurements[key]['in'][i]
+		if 1.0 * all_measurements[key]['out'][i] / all_measurements[key]['in'][i] > max:
+			max = 1.0 * all_measurements[key]['out'][i] / all_measurements[key]['in'][i]
 	print('  "lower":' + str(min) + ',\\')
 	print('  "upper":' + str(max) + ',\\')
 	print('}')
--- reoptimizer_benchmark_suite.sh
+++ reoptimizer_benchmark_suite.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 
-for properties in rheem-benchmark-no-reopt.properties rheem-benchmark-aggresive-reopt.properties 
+logfolder=suite-logs-neu
+platforms=spark,java
+
+for props in benchmark.properties # rheem-benchmark-aggresive-reopt.properties 
 do
+	properties=$props
 	echo $properties
 
-	base_command="timeout --kill-after 1m 1h java -Xmx4g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.2-bin-hadoop2.6/lib/spark-assembly-1.6.2-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro-0.2.2-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro-0.2.2-SNAPSHOT-distro/rheem-distro-0.2.2-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
+	base_command="timeout --kill-after 1m 30m time java -Xmx8g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.2-bin-hadoop2.6/lib/spark-assembly-1.6.2-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT-distro/rheem-distro_2.11-0.3.1-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/rheem-benchmark/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
+	
+	echo "TPC-H"
+	for filename in #tpch-10g.properties tpch-5g.properties tpch.properties
+	do
+		echo $filename
+		for query in Q3File #Q3Hybrid Q1 Q3
+		do
+			echo $query
+			date
+			this_command="tpch.TpcH 'exp(KMeans-$filename-$properties)' $platforms hdfs://tenemhead2/data/$filename $query 0.2 2>&1 | gzip > /home/jonas.kemper/$logfolder/TpcH-$filename-$query-$properties.gz"
+			eval "$base_command$this_command"
+			sleep 5
+		done
+	done
 	
 	echo "KMeans"
-	for filename in 1k 10k 100k 1m 5m 10m
+	for filename in #1k 10k 100k 1m 5m 10m
 		do
 		path=hdfs://tenemhead2/data/2dpoints/kmeans_points_
 		input_file=$path$filename.txt
 		date
 		echo $input_file
-		this_command="kmeans.Kmeans 'exp(KMeans-$filename-$properties)' java,spark $input_file 20 100 | gzip > /home/jonas.kemper/suite-logs/KMeans-$filename-$properties.gz"
+		this_command="kmeans.Kmeans 'exp(KMeans-$filename-$properties)' $platforms $input_file 20 100 2>&1 | gzip > /home/jonas.kemper/$logfolder/KMeans-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		sleep 5
 	done
 
 	echo "SGD"
-	for filename in 0.01m 0.1m 0.5m 1m 1250k 1500k 2m 3m 4m
+	for filename in #0.01m 0.1m 0.5m 1m 1250k 1500k 2m 3m 4m
 		do
 		path=hdfs://tenemhead2/data/HIGGS/higgs-train-
 		input_file=$path$filename.csv
@@ -59,31 +77,31 @@ do
 		dataset_size=4000000 
 		fi                   
 		echo "dataset size: $dataset_size"
-		this_command="sgd.SGD 'exp(SGD-$filename-$properties)' java,spark regular $input_file $dataset_size 28 100 0.001 10 | gzip > /home/jonas.kemper/suite-logs/SGD-$filename-$properties.gz"
+		this_command="sgd.SGD 'exp(SGD-$filename-$properties)' $platforms regular $input_file $dataset_size 28 100 0.001 10 2>&1 | gzip > /home/jonas.kemper/$logfolder/SGD-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		sleep 5
 	done
 	
 	echo "SimWords"
-	for filename in #01pc 2pc 3pc
+	for filename in 01pc #2pc 3pc
 		do
 		path=hdfs://tenemhead2/data/text/dbpedia-2015-10/long_abstracts_en_
 		input_file=$path$filename.txt
 		echo $input_file
 		date
-		this_command="simwords.SimWords 'exp(SimWords-$filename-$properties)' java,spark $input_file 5 5 5 5 | gzip > /home/jonas.kemper/suite-logs/SimWords-$filename-$properties.gz"
+		this_command="simwords.SimWords 'exp(SimWords-$filename-$properties)' $platforms $input_file 5 5 5 5 2>&1 | gzip > /home/jonas.kemper/$logfolder/SimWords-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		sleep 5
 	done
 	
 	echo "Wordcount"
-	for filename in #01pc 2pc 3pc 10pc 25pc 50pc
+	for filename in 01pc 2pc 3pc 10pc 25pc 50pc
 		do
 		path=hdfs://tenemhead2/data/text/dbpedia-2015-10/long_abstracts_en_
 		input_file=$path$filename.txt
 		echo $input_file
 		date
-		this_command="wordcount.WordCountScala 'exp(Wordcount-$filename-$properties)' java,spark $input_file | gzip > /home/jonas.kemper/suite-logs/Wordcount-$filename-$properties.gz"
+		this_command="wordcount.WordCountScala 'exp(Wordcount-$filename-$properties)' $platforms $input_file 2>&1 | gzip > /home/jonas.kemper/$logfolder/Wordcount-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		sleep 5
 	done
@@ -98,7 +116,7 @@ do
 		o_input_file=$o_path$filename
 		echo $o_input_file
 		date
-		this_command="sindy.Sindy 'exp(SINDY-$filename-$properties)' java,spark , '$l_input_file;$o_input_file' | gzip > /home/jonas.kemper/suite-logs/SINDY-$filename-$properties.gz"
+		this_command="sindy.Sindy 'exp(SINDY-$filename-$properties)' $platforms , '$l_input_file;$o_input_file' 2>&1 | gzip > /home/jonas.kemper/$logfolder/SINDY-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		sleep 5
 	done
