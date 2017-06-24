355dfef88beb6be227b0322912a893ab9749fa04
jonas.kemper <jonas.kemper.biz@gmail.com>
2017-06-24 14:29:57 +0200

    update

--- reoptimizer_benchmark_suite.sh
+++ reoptimizer_benchmark_suite.sh
@@ -2,17 +2,17 @@
 echo "Bash version ${BASH_VERSION}..."
 
 logfolder=$1
-platforms=spark,java
+platforms=spark,java,basic-graph,java-graph,spark-graph
 
-for props in benchmark-tenem.properties # rheem-benchmark-aggresive-reopt.properties 
+for props in benchmark-thor.properties # rheem-benchmark-aggresive-reopt.properties 
 do
 	properties=$props
 	echo $properties
 
-	base_command="timeout --kill-after=1m --signal=9 32m time java -Xmx8g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.3_2.11/assembly/target/scala-2.11/spark-assembly-1.6.3-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT-distro/rheem-distro_2.11-0.3.1-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/MA-Scripts/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
+	base_command="timeout --kill-after=1m 64m java -Xmx8g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.3_2.11/assembly/target/scala-2.11/spark-assembly-1.6.3-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT-distro/rheem-distro_2.11-0.3.1-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/MA-Scripts/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
 	
 	echo "TPC-H"
-	for filename in #0_01 0_1 0_5 1 2 5 10 20 50 100
+	for filename in 0_01 0_1 0_5 1 2 5 10 20 50 100
 		do
 		echo $filename
 		for query in Q3File #Q3Hybrid Q1 Q3
@@ -26,7 +26,7 @@ do
 	done
 	
 	echo "KMeans"
-	for filename in #5GB.csv 500MB.csv 4GB.csv 3GB.csv 2GB.csv 250MB.csv 1MB.csv 1GB.csv 125MB.csv 10MB.csv 
+	for filename in 5GB.csv 500MB.csv 4GB.csv 3GB.csv 2GB.csv 250MB.csv 1MB.csv 1GB.csv 125MB.csv 10MB.csv 
 		do
 		path=hdfs://thor01/data/csv/kmeans-generator/2d-
 		input_file=$path$filename
@@ -38,7 +38,7 @@ do
 	done
 
 	echo "SGD"
-	for filename in #0.01m 0.1m 0.5m 1m 1250k 1500k 2m 3m 4m
+	for filename in 0.01m 0.1m 0.5m 1m 1250k 1500k 2m 3m 4m
 		do
 		path=hdfs://thor01/data/HIGGS/higgs-train-
 		input_file=$path$filename.csv
@@ -83,7 +83,7 @@ do
 	done
 	
 	echo "SimWords"
-	for filename in #0_0625 #0_125 0_25 0_5 2 4 8
+	for filename in 0_0625 #0_125 0_25 0_5 2 4 8
 		do
 		path=hdfs://thor01/data/text/dbpedia/dbpedia-2016-04/long_abstracts_en.
 		input_file=$path$filename.txt
