d58c857e3f3aa7270d8565733e72a67b3c7bde66
Jonas <jonas.kemper.biz@gmail.com>
2017-07-07 12:32:02 +0200

    cleaner

--- reoptimizer_benchmark_suite.sh
+++ reoptimizer_benchmark_suite.sh
@@ -2,17 +2,24 @@
 echo "Bash version ${BASH_VERSION}..."
 
 logfolder=$1
+exec_mode=$2
+timeout_after=$3
+properties_file=$4
+
 platforms=spark,java,basic-graph,java-graph,spark-graph
 
-for props in benchmark-thor.properties # rheem-benchmark-aggresive-reopt.properties 
+for props in $properties_file # rheem-benchmark-aggresive-reopt.properties 
 do
 	properties=$props
 	echo $properties
 
-	base_command="timeout --kill-after=1m 32m java -Xmx8g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.3_2.11/assembly/target/scala-2.11/spark-assembly-1.6.3-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT-distro/rheem-distro_2.11-0.3.1-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/MA-Scripts/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
+	base_command="timeout --kill-after=1m timeout_after java -Xmx8g -cp /home/jonas.kemper/rheem-benchmark/target/*:/opt/spark/spark-1.6.3_2.11/assembly/target/scala-2.11/spark-assembly-1.6.3-hadoop2.6.0.jar:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-store/0.1.2-SNAPSHOT/*:/home/jonas.kemper/.m2/repository/de/hpi/isg/profiledb-instrumentation/0.1.2-SNAPSHOT/*:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT.jar:/home/jonas.kemper/rheem/rheem-distro/target/rheem-distro_2.11-0.3.1-SNAPSHOT-distro/rheem-distro_2.11-0.3.1-SNAPSHOT/* -Drheem.configuration=file:/home/jonas.kemper/MA-Scripts/$properties -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dlog4j.configuration=file:/home/jonas.kemper/log4j.properties -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.util.JuelUtils$JuelFunction=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.optimizer.DefaultOptimizationContext=debug -Dorg.slf4j.simpleLogger.log.org.apache.spark=debug -Dorg.slf4j.simpleLogger.log.org.qcri.rheem.core.api.Job=debug org.qcri.rheem.apps."
 	
+    
+if [ $1 = "validation" ]; then
+    
 	echo "TPC-H"
-	for filename in 0_01 0_1 0_5 1 2 5 10 20 50 100
+	for filename in 100 20 5 1 0_1
 		do
 		echo $filename
 		for query in Q3File #Q3Hybrid Q1 Q3
@@ -26,58 +33,13 @@ do
 	done
 	
 	echo "KMeans"
-	for filename in 5GB.csv 500MB.csv 4GB.csv 3GB.csv 2GB.csv 250MB.csv 1MB.csv 1GB.csv 125MB.csv 10MB.csv 
+	for filename in 500MB.csv 3GB.csv 1GB.csv 125MB.csv 10MB.csv
 		do
 		path=hdfs://thor01/data/csv/kmeans-generator/2d-
 		input_file=$path$filename
 		date
 		echo $input_file
 		this_command="kmeans.Kmeans 'exp(KMeans-$filename-$properties)' $platforms $input_file 20 100 2>&1 | gzip > /home/jonas.kemper/$logfolder/KMeans-$filename-$properties.gz"
-		eval "$base_command$this_command"
-		#sleep 1
-	done
-
-	echo "SGD"
-	for filename in #0.01m 0.1m 0.5m 1m 1250k 1500k 2m 3m 4m
-		do
-		path=hdfs://thor01/data/HIGGS/higgs-train-
-		input_file=$path$filename.csv
-		echo $input_file
-	 	date	
-		if [ $filename = 0.01m ]
-		then
-		dataset_size=10000
-		fi
-		if [ $filename = 0.1m ]
-		then
-		dataset_size=100000
-		fi
-		if [ $filename = 1m ]
-		then
-		dataset_size=1000000
-		fi
-		if [ $filename = 1250k ]
-		then
-		dataset_size=1250000
-		fi
-		if [ $filename = 1500k ]
-		then
-		dataset_size=1500000
-		fi
-		if [ $filename = 2m ]
-		then
-		dataset_size=2000000
-		fi
-		if [ $filename = 3m ]
-		then                 
-		dataset_size=3000000 
-		fi                   
-		if [ $filename = 4m ]
-		then                 
-		dataset_size=4000000 
-		fi                   
-		echo "dataset size: $dataset_size"
-		this_command="sgd.SGD 'exp(SGD-$filename-$properties)' $platforms regular $input_file $dataset_size 28 100 0.001 10 2>&1 | gzip > /home/jonas.kemper/$logfolder/SGD-$filename-$properties.gz"
 		eval "$base_command$this_command"
 		#sleep 1
 	done
@@ -123,7 +85,7 @@ do
 	done
 
 	echo "CrocoPR"
-	for filename in page_links_en_uris_de-0_01.ttl page_links_en-0_125.ttl page_links_en-0_25.ttl page_links_en-0_5.ttl page_links_en.ttl page_links_en_uris_de-0_001.ttl page_links_en_uris_de-0_01.ttl page_links_en_uris_de-0_125.ttl page_links_en_uris_de-0_25.ttl page_links_en_uris_de-0_5.ttl page_links_en_uris_de.ttl page_links_en_uris_fr.ttl page_links_en_uris_pt.ttl 
+	for filename in page_links_en_uris_de-0_01.ttl page_links_en-0_125.ttl page_links_en-0_25.ttl page_links_en_uris_de.ttl page_links_en_uris_fr.ttl page_links_en_uris_pt.ttl page_links_en_uris_de-0_5.ttl
 		do
 		l_path=hdfs://thor01/data/rdf/dbpedia/dbpedia-2016-04/
 		l_input_file=$l_path$filename
@@ -137,3 +99,80 @@ do
 		#sleep 1
 	done
 done	
+
+fi
+
+
+if [ $1 = "training" ]; then
+    
+	echo "TPC-H"
+	for filename in 0_01 0_5 2 10 50
+		do
+		echo $filename
+		for query in Q3File #Q3Hybrid Q1 Q3
+		do
+			echo $query
+			date
+			this_command="tpch.TpcH 'exp(KMeans-$filename-$properties)' $platforms hdfs://thor01/data/csv/TPC-H/tpch_props_$filename.txt $query 0.2 2>&1 | gzip > /home/jonas.kemper/$logfolder/TpcH-$filename-$query-$properties.gz"
+			eval "$base_command$this_command"
+			sleep 5
+		done
+	done
+	
+	echo "KMeans"
+	for filename in 5GB.csv 4GB.csv 2GB.csv 1MB.csv 250MB.csv
+		do
+		path=hdfs://thor01/data/csv/kmeans-generator/2d-
+		input_file=$path$filename
+		date
+		echo $input_file
+		this_command="kmeans.Kmeans 'exp(KMeans-$filename-$properties)' $platforms $input_file 20 100 2>&1 | gzip > /home/jonas.kemper/$logfolder/KMeans-$filename-$properties.gz"
+		eval "$base_command$this_command"
+		sleep 5
+	done
+	
+	echo "Wordcount"
+	for filename in x
+		do
+		path=hdfs://thor01/data/csv/CENSUS/CENSUS.csv
+		input_file=$path
+		echo $input_file
+		date
+		this_command="wordcount.WordCountScala 'exp(Wordcount-$filename-$properties)' $platforms $input_file 2>&1 | gzip > /home/jonas.kemper/$logfolder/Wordcount-$filename-$properties.gz"
+		eval "$base_command$this_command"
+		sleep 5
+	done
+	
+	echo "SINDY"
+	for filename in x
+		do
+		l_path=hdfs://thor01/data/csv/WIKIPEDIA
+		l_path2=/IMAGELINKS.csv
+		l_input_file=$l_path$l_path2
+		echo $l_input_file
+		o_path2=/IMAGE.csv
+		o_input_file=$l_path$o_path2
+		echo $o_input_file
+		date
+		this_command="sindy.Sindy 'exp(SINDY-$filename-$properties)' $platforms , '$l_input_file;$o_input_file' 2>&1 | gzip > /home/jonas.kemper/$logfolder/SINDY-$filename-$properties.gz"
+		eval "$base_command$this_command"
+		sleep 5
+	done
+
+	echo "CrocoPR"
+	for filename in page_links_en-0_5.ttl page_links_en.ttl page_links_en_uris_de-0_001.ttl page_links_en_uris_de-0_01.ttl page_links_en_uris_de-0_125.ttl page_links_en_uris_de-0_25.ttl page_links_en_uris_de.ttl page_links_en_uris_fr.ttl page_links_en_uris_pt.ttl 
+		do
+		l_path=hdfs://thor01/data/rdf/dbpedia/dbpedia-2016-04/
+		l_input_file=$l_path$filename
+		echo $l_input_file
+		o_path2=page_links_en_uris_de-0_001.ttl
+		o_input_file=$l_path$o_path2
+		echo $o_input_file
+		date
+		this_command="crocopr.CrocoPR 'exp(SINDY-$filename-$properties)' $platforms $l_input_file $o_input_file 3 2>&1 | gzip > /home/jonas.kemper/$logfolder/CrocoPR-$filename-$properties.gz"
+		eval "$base_command$this_command"
+		#sleep 1
+	done
+done	
+    
+fi
--- start_new_benchmarking_suite_run.sh
+++ start_new_benchmarking_suite_run.sh
@@ -1,16 +1,22 @@
-mkdir ~/suite-logs-thor-wout-$1
-mkdir ~/suite-logs-thor-with-$1
+mkdir ~/suite-logs-thor-baseline-$1
 mkdir ~/suite-logs-thor-training-$1
-mkdir ~/suite-logs-thor-only-$1
+mkdir ~/suite-logs-thor-validation-$1
 
 mv ~/MA-Scripts/benchmark-thor.properties ~/MA-Scripts/benchmark-thor_BACKUP_$1.properties
-cp ~/MA-Scripts/benchmark-thor-blank.properties ~/MA-Scripts/benchmark-thor.properties
 mv ~/.rheem/executions.json ~/.rheem/executions_backup_$1.json
-sed -i "s/June24-15uhr/$1/g" ~/MA-Scripts/benchmark-thor.properties
 
-sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-wout-$1 && sh ~/MA-Scripts/create_and_copy_select_repo.sh && sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-with-$1 && mv ~/.rheem/executions.json ~/.rheem/executions_before_training-$1.json && mv ~/MA-Scripts/benchmark-thor.properties ~/MA-Scripts/benchmark-thor_before_training-$1.properties 
-
-
-#cp ~/MA-Scripts/benchmark-thor-blank.properties ~/MA-Scripts/benchmark-thor.properties && sh ~/MA-Scripts/reoptimizer_benchmark_suite_training.sh suite-logs-thor-training-$1 && sh ~/MA-Scripts/create_and_copy_select_repo.sh && sh ~/MA-Scripts/reoptimizer_benchmark_suite_only_wc_sindy.sh suite-logs-thor-only-$1
+cp ~/MA-Scripts/benchmark-thor-blank.properties ~/MA-Scripts/benchmark-thor-baseline-$1.properties
+cp ~/MA-Scripts/benchmark-thor-blank.properties ~/MA-Scripts/benchmark-thor-training-$1.properties
+cp ~/MA-Scripts/benchmark-thor-blank.properties ~/MA-Scripts/benchmark-thor-validation-$1.properties
 
 
+sed -i "s/June24-15uhr/$1/g" ~/MA-Scripts/benchmark-thor-*-$1.properties.properties
+
+
+sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-baseline-$1 validation $2 benchmark-thor-baseline-$1.properties
+mv ~/.rheem/executions.json ~/.rheem/executions_backup_baseline_$1.json
+sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-baseline-$1 training $2 benchmark-thor-baseline-$1.properties
+sh ~/MA-Scripts/create_and_copy_select_repo.sh benchmark-thor-validation-$1.properties
+mv ~/.rheem/executions.json ~/.rheem/executions_backup_training_$1.json
+sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-baseline-$1 validation $2 benchmark-thor-baseline-$1.properties
+mv ~/.rheem/executions.json ~/.rheem/executions_backup_validation_$1.json
