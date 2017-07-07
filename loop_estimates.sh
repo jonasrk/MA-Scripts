390917f0de7640d6023b7f7462ec44ad2bb1a590
Jonas <jonas.kemper.biz@gmail.com>
2017-07-07 11:59:00 +0200

    update

--- loop_estimates.sh
+++
@@ -1,4 +0,0 @@
-while read p; do
-  sh parse_estimates.sh "$p"
-  echo $p
-  done <$1
--- parse_reopt.sh
+++
@@ -1,9 +0,0 @@
-for filename in *
-	do
-		IFS='-' read -ra ADDR <<< $filename
-		for i in "${ADDR[@]}" 
-			do
-		    		 echo $i
-			done
-		zgrep "* Execution" $filename | head -1 | sed -e 's/^.*- //g'
-	done
