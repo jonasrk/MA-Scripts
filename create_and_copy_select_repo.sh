c41285882c5f28bd86978b0ffbd9103596565fb4
jonas.kemper <jonas.kemper.biz@gmail.com>
2017-07-10 11:53:40 +0200

    8min minmax

--- create_and_copy_select_repo.sh
+++ create_and_copy_select_repo.sh
@@ -1,3 +1,3 @@
-sh /home/jonas.kemper/MA-Scripts/extract_udf_cards.sh $2 > /home/jonas.kemper/MA-Scripts/extracted_udf_cards.txt && python3 /home/jonas.kemper/MA-Scripts/generate_select_repo.py /home/jonas.kemper/MA-Scripts/extracted_udf_cards.txt > /home/jonas.kemper/MA-Scripts/select_repo.txt
+sh /home/jonas.kemper/MA-Scripts/extract_udf_cards.sh $2 > /home/jonas.kemper/MA-Scripts/extracted_udf_cards.txt && python3 /home/jonas.kemper/MA-Scripts/$3.py /home/jonas.kemper/MA-Scripts/extracted_udf_cards.txt > /home/jonas.kemper/MA-Scripts/select_repo.txt
 
 cat /home/jonas.kemper/MA-Scripts/select_repo.txt >> /home/jonas.kemper/MA-Scripts/$1
--- start_new_benchmarking_suite_run.sh
+++ start_new_benchmarking_suite_run.sh
@@ -4,6 +4,7 @@ filename=$2
 if [ $exec_mode = "all" ]; then
 
     timeout=$3
+    select_repo_generator=$4
 
     mkdir ~/suite-logs-thor-baseline-$filename
     mkdir ~/suite-logs-thor-training-$filename
@@ -24,7 +25,7 @@ if [ $exec_mode = "all" ]; then
     
     sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-training-$filename training $timeout benchmark-thor-baseline-$filename.properties
     mv ~/.rheem/executions.json ~/.rheem/executions_backup_training_$filename.json
-    sh ~/MA-Scripts/create_and_copy_select_repo.sh benchmark-thor-validation-$filename.properties ~/.rheem/executions_backup_training_$filename.json
+    sh ~/MA-Scripts/create_and_copy_select_repo.sh benchmark-thor-validation-$filename.properties ~/.rheem/executions_backup_training_$filename.json $select_repo_generator
     
     sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-validation-$filename validation $timeout benchmark-thor-validation-$filename.properties
     mv ~/.rheem/executions.json ~/.rheem/executions_backup_validation_$filename.json
@@ -34,15 +35,18 @@ fi
 if [ $exec_mode = "regenarate_repo" ]; then
 
     training_log=$3
+    select_repo_generator=$4
+
     cp ~/MA-Scripts/benchmark-thor-blank-noexec.properties ~/MA-Scripts/benchmark-thor-validation-$filename.properties
     sed -i "s/June24-15uhr/$filename/g" ~/MA-Scripts/benchmark-thor-*-$filename.properties
-    sh ~/MA-Scripts/create_and_copy_select_repo.sh benchmark-thor-validation-$filename.properties $training_log
+    sh ~/MA-Scripts/create_and_copy_select_repo.sh benchmark-thor-validation-$filename.properties $training_log $select_repo_generator
 
 fi
 
 if [ $exec_mode = "validate-noexec" ]; then
 
     timeout=$3
+
     mkdir ~/suite-logs-thor-validation-$filename
     sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-validation-$filename validation $timeout benchmark-thor-validation-$filename.properties
 
@@ -61,7 +65,7 @@ if [ $exec_mode = "generate-baseline-and-training-logs" ]; then
     sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-baseline-$filename validation $timeout benchmark-thor-baseline-$filename.properties
     mv ~/.rheem/executions.json ~/.rheem/executions_backup_baseline_$filename.json
     
-    sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-training-$filename training $timeout benchmark-thor-baseline-$filename.properties
+    sh ~/MA-Scripts/reoptimizer_benchmark_suite.sh suite-logs-thor-training-$filename training $timeout benchmark-thor-training-$filename.properties
     mv ~/.rheem/executions.json ~/.rheem/executions_backup_training_$filename.json
 
 
