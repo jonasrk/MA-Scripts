129a0ca49b0084b8542456132e7517c65ca7c33d
Jonas <jonas.kemper.biz@gmail.com>
2017-07-10 11:54:11 +0200

    8min minmax

--- get_and_csv_benchmark_results.sh
+++ get_and_csv_benchmark_results.sh
@@ -11,13 +11,11 @@ scp -r jonas.kemper@thor01:/home/jonas.kemper/MA-Scripts/\*$1\* .
 scp -r jonas.kemper@thor01:/home/jonas.kemper/.rheem/\*$1\* .
 
 
-cd "/Users/jonas/suite-logs/$1/suite-logs-thor-baseline-$1"
-sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_baseline-$1.csv
-cd "/Users/jonas/suite-logs/$1/suite-logs-thor-training-$1"
-sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_training-$1.csv
+cd "/Users/jonas/suite-logs/$1/suite-logs-thor-baseline-$1" && sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_baseline-$1.csv
 
-cd "/Users/jonas/suite-logs/$1/suite-logs-thor-validation-$1"
-sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_validation-$1.csv
+cd "/Users/jonas/suite-logs/$1/suite-logs-thor-training-$1" && sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_training-$1.csv
+
+cd "/Users/jonas/suite-logs/$1/suite-logs-thor-validation-$1" && sh ~/MA-Scripts/extract_last_estimates_from_log.sh | sed "s/\.\/SINDY-//" | sed "s/-benchmark-thor.*\.properties\.gz//g" | sed "s/.\/CrocoPR-//" | sed "s/\.\/KMeans-//g" | sed "s/\.csv//" | sed "s/\.\/TpcH-//" | sed "s/-Q3File//" | sed "s/\.\/Wordcount-//" > est_cards_validation-$1.csv
 
 mkdir /Users/jonas/Google\ Drive/suite-logs/$1
 
