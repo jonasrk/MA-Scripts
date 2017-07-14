f818750a75d48109e23e3659a6685a0401acb348
Jonas <jonas.kemper.biz@gmail.com>
2017-07-14 15:56:50 +0200

    backup scripts

--- generate_select_repo.py
+++ generate_select_repo.py
@@ -2,107 +2,159 @@ import sys, json
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-# READ FILE
-with open(sys.argv[1]) as f:
-    content = f.readlines()
-    content = [x.strip() for x in content]
 
-all_measurements = {}
+def read_and_process_json(file):
+    # READ FILE
+    with open(file) as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
 
-for line in content:
-    parsed = json.loads(line)
-    for execGroup in parsed['execGroups']:
-        if ('selectivityKey' in execGroup['ctx']):
-            selectivityKey = execGroup['ctx']['selectivityKey']
-            if ('my.udf.' in selectivityKey):
-                key = selectivityKey.split('-')[0]
-                if key not in all_measurements:
-                    all_measurements[key] = {}
-                    all_measurements[key]['in'] = []
-                    all_measurements[key]['out'] = []
-                all_measurements[key]['in'].append(execGroup['ctx']['inCards'])
-                all_measurements[key]['out'].append(execGroup['ctx']['outCards'])
+    all_measurements = {}
 
-for key in all_measurements:
+    for line in content:
+        parsed = json.loads(line)
+        for execGroup in parsed['execGroups']:
+            if ('selectivityKey' in execGroup['ctx']):
+                selectivityKey = execGroup['ctx']['selectivityKey']
+                if ('my.udf.' in selectivityKey):
+                    key = selectivityKey.split('-')[0]
+                    if key not in all_measurements:
+                        all_measurements[key] = {}
+                        all_measurements[key]['in'] = []
+                        all_measurements[key]['out'] = []
+                    all_measurements[key]['in'].append(execGroup['ctx']['inCards'])
+                    all_measurements[key]['out'].append(execGroup['ctx']['outCards'])
 
-    create_this_key = False
+    for key in all_measurements:
 
-    xdata = []
-    ydata = []
+        create_this_key = False
 
-    min = sys.float_info.max
-    max = 0.0
-    max_card = 0
+        xdata = []
+        ydata = []
 
-    for i in range(0, len(all_measurements[key]['in'])):
-        in_lower_sum = 0
-        in_upper_sum = 0
-        use_measurement = True
-        for j in range(0, len(all_measurements[key]['in'][i])):
-            if all_measurements[key]['in'][i][j]['confidence'] != 1:
-                use_measurement = False
-            if all_measurements[key]['in'][i][j]['confidence'] == 1:
-                if all_measurements[key]['in'][i][j]['lowerBound'] < 0 or in_upper_sum + \
-                        all_measurements[key]['in'][i][j]['upperBound'] < 0:
+        min = sys.float_info.max
+        max = 0.0
+        max_card = 0
+
+        for i in range(0, len(all_measurements[key]['in'])):
+            in_lower_sum = 0
+            in_upper_sum = 0
+            use_measurement = True
+            for j in range(0, len(all_measurements[key]['in'][i])):
+                if all_measurements[key]['in'][i][j]['confidence'] != 1:
                     use_measurement = False
-                in_lower_sum = in_lower_sum + all_measurements[key]['in'][i][j]['lowerBound']
-                in_upper_sum = in_upper_sum + all_measurements[key]['in'][i][j]['upperBound']
-        out_lower_sum = 0
-        out_upper_sum = 0
-        for j in range(0, len(all_measurements[key]['out'][i])):
-            if all_measurements[key]['out'][i][j]['confidence'] != 1:
-                use_measurement = False
-            if all_measurements[key]['out'][i][j]['confidence'] == 1:
-                if all_measurements[key]['out'][i][j]['lowerBound'] < 0 or in_upper_sum + \
-                        all_measurements[key]['out'][i][j]['upperBound'] < 0:
+                if all_measurements[key]['in'][i][j]['confidence'] == 1:
+                    if all_measurements[key]['in'][i][j]['lowerBound'] < 0 or in_upper_sum + \
+                            all_measurements[key]['in'][i][j]['upperBound'] < 0:
+                        use_measurement = False
+                    in_lower_sum = in_lower_sum + all_measurements[key]['in'][i][j]['lowerBound']
+                    in_upper_sum = in_upper_sum + all_measurements[key]['in'][i][j]['upperBound']
+            out_lower_sum = 0
+            out_upper_sum = 0
+            for j in range(0, len(all_measurements[key]['out'][i])):
+                if all_measurements[key]['out'][i][j]['confidence'] != 1:
                     use_measurement = False
-                out_lower_sum = out_lower_sum + all_measurements[key]['out'][i][j]['lowerBound']
-                out_upper_sum = out_upper_sum + all_measurements[key]['out'][i][j]['upperBound']
-        if use_measurement:
-            selectivity = 1.0 * out_lower_sum / in_lower_sum
-            if selectivity < min:
-                create_this_key = True
-                min = selectivity
-            if selectivity > max:
-                max = selectivity
-            selectivity = 1.0 * (out_lower_sum * out_upper_sum)**1/2 / (in_lower_sum * in_upper_sum)**1/2
-            xdata.append((in_lower_sum * in_upper_sum)**1/2) # TODO JRK: geometric mean right choice? better distinct lower and upper?
-            if (in_lower_sum * in_upper_sum)**1/2 > max_card:
-                max_card = (in_lower_sum * in_upper_sum)**1/2
-            ydata.append(selectivity)
+                if all_measurements[key]['out'][i][j]['confidence'] == 1:
+                    if all_measurements[key]['out'][i][j]['lowerBound'] < 0 or in_upper_sum + \
+                            all_measurements[key]['out'][i][j]['upperBound'] < 0:
+                        use_measurement = False
+                    out_lower_sum = out_lower_sum + all_measurements[key]['out'][i][j]['lowerBound']
+                    out_upper_sum = out_upper_sum + all_measurements[key]['out'][i][j]['upperBound']
+            if use_measurement:
+                selectivity = 1.0 * out_lower_sum / in_lower_sum
+                if selectivity < min:
+                    create_this_key = True
+                    min = selectivity
+                if selectivity > max:
+                    max = selectivity
+                selectivity = 1.0 * (out_lower_sum * out_upper_sum)**1/2 / (in_lower_sum * in_upper_sum)**1/2
+                xdata.append((in_lower_sum * in_upper_sum)**(1/2)) # TODO JRK: geometric mean right choice? better distinct lower and upper?
+                if (in_lower_sum * in_upper_sum)**(1/2) > max_card:
+                    max_card = (in_lower_sum * in_upper_sum)**(1/2)
+                    all_measurements[key]['max_card'] = max_card
+                ydata.append(selectivity)
 
-    xdata = np.array(xdata)
-    ydata = np.array(ydata)
+        all_measurements[key]['xdata'] = xdata
+        all_measurements[key]['ydata'] = ydata
 
-
-
-    try:
-        predictor = LinearRegression(n_jobs=-1)
-        predictor.fit(X=[[x] for x in xdata], y=ydata)
-
-        coefficient = predictor.coef_
-        intercept = predictor.intercept_
-    except ValueError:
-        coefficient = [0.0]
-        intercept = [0.0]
+        xdata = np.array(xdata)
+        ydata = np.array(ydata)
 
 
-    if sys.argv[2] == "generate_plots":
-        import matplotlib.pyplot as plt
-        my_dpi = 96
-        plt.figure(figsize=(200 / my_dpi, 150 / my_dpi), dpi=my_dpi)
 
-        plt.scatter(xdata, ydata)
-        plt.title(key)
-        x = np.linspace(0, max_card, 100)
-        y = x * coefficient + intercept
-        plt.plot(x, y, "r--")
-        plt.savefig(home + '/Google Drive/suite-logs/' + sys.argv[2] +  '/' + sys.argv[2] + '-' + key + '.png', dpi=my_dpi)
-        plt.close()
+        try:
+            predictor = LinearRegression(n_jobs=-1)
+            predictor.fit(X=[[x] for x in xdata], y=ydata)
 
-    all_measurements[key]['coefficient'] = coefficient
-    all_measurements[key]['intercept'] = intercept
+            coefficient = predictor.coef_
+            intercept = predictor.intercept_
+        except ValueError:
+            coefficient = [0.0]
+            intercept = [0.0]
 
-    if create_this_key:
-        print(key + " = {" + '  "p":1, ' + '  "lower":' + str(min) + ',' + '  "upper":' + str(max) + ', "coeff":' + str(all_measurements[key]['coefficient'][0]) + ', "intercept":' + str(all_measurements[key]['intercept']) + '}')
+        all_measurements[key]['coefficient'] = coefficient
+        all_measurements[key]['intercept'] = intercept
 
+        if create_this_key:
+            print(key + " = {" + '  "p":1, ' + '  "lower":' + str(min) + ',' + '  "upper":' + str(max) + ', "coeff":' + str(all_measurements[key]['coefficient'][0]) + ', "intercept":' + str(all_measurements[key]['intercept']) + '}')
+
+    f.close()
+
+
+    return all_measurements
+
+
+all_measurements1 = read_and_process_json(file=sys.argv[1])
+all_measurements2 = read_and_process_json(file=sys.argv[2])
+
+
+generate_plots=sys.argv[4]
+date_id = sys.argv[3]
+
+if generate_plots == "generate_plots":
+    import matplotlib.pyplot as plt
+    import csv
+
+    for key in all_measurements2:
+
+        if all_measurements2[key]['coefficient'] != 0.0:
+            my_dpi = 96
+            plt.figure(figsize=(200 / my_dpi, 150 / my_dpi), dpi=my_dpi)
+            ax = plt.gca()
+            ax.scatter(all_measurements2[key]['xdata'], all_measurements2[key]['ydata'], c="red", marker=(5, 2))
+            ax.scatter(all_measurements1[key]['xdata'], all_measurements1[key]['ydata'], c="blue", marker='+')
+            default_estimator_x = []
+            default_estimator_y = []
+
+            with open('/Users/jonas/Google Drive/suite-logs/July13-23uhr10/est_cards_baseline-July13-23uhr10.csv',
+                      'rt') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+                for row in spamreader:
+                    if (row[1].split('-')[0] == key):
+                        # print(row[1].split('-')[0])
+                        # print(str(int(row[2].replace(',', ''))))
+                        # print(str(int(row[5].replace(',', '')) / int(row[2].replace(',', '')) * 1.0))
+                        # print(str(int(row[6].replace(',', '')) / int(row[3].replace(',', '')) * 1.0))
+                        default_estimator_x.append((int(row[2].replace(',', '')) * int(row[3].replace(',', ''))) ** (1/2))
+                        default_estimator_y.append(int(row[5].replace(',', '')) / int(row[2].replace(',', '')) * 1.0)
+                        default_estimator_x.append((int(row[2].replace(',', '')) * int(row[3].replace(',', ''))) ** (1 / 2))
+                        default_estimator_y.append(int(row[6].replace(',', '')) / int(row[3].replace(',', '')) * 1.0)
+            csvfile.close()
+
+            ax.scatter(default_estimator_x, default_estimator_y, c="green", marker=(3, 2))
+
+            max_baseline=0
+            if len(default_estimator_x) > 0:
+                max_baseline = max(default_estimator_x)
+            max_card = max([all_measurements2[key]['max_card'], all_measurements1[key]['max_card'], max_baseline])
+
+            plt.title(key)
+            x = np.linspace(0, max_card, 100)
+            y = x * all_measurements2[key]['coefficient'] + all_measurements2[key]['intercept']
+            ax.set_xscale('log')
+            plt.plot(x, y, "r--")
+            from os.path import expanduser
+
+            home = expanduser("~")
+            plt.savefig(home + '/Google Drive/suite-logs/' + date_id + '/linear_training_validation-' + date_id + '-' + key + '.png', dpi=my_dpi)
+            plt.close()
