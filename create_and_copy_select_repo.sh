1aa7b9fbaeec84b6613e66040cdaa1e217e6241d
jonas.kemper <jonas.kemper.biz@gmail.com>
2017-07-15 16:37:42 +0200

    thor update

--- create_and_copy_select_repo.sh
+++ create_and_copy_select_repo.sh
@@ -1,3 +1,3 @@
-python3 /home/jonas.kemper/MA-Scripts/$3.py $2 $4 > /home/jonas.kemper/MA-Scripts/select_repo.txt
+python3 /home/jonas.kemper/MA-Scripts/$3.py none $2 $4 dont_generate_plots > /home/jonas.kemper/MA-Scripts/select_repo.txt
 
 cat /home/jonas.kemper/MA-Scripts/select_repo.txt >> /home/jonas.kemper/MA-Scripts/$1
--- generate_execution_logs.sh
+++ generate_execution_logs.sh
@@ -1,4 +1,4 @@
-i=32
+i=64
 
 while true; do
 
